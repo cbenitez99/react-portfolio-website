c9ba073d56e63d344d9ecc28d8493ece1103158d
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-04-14 13:55:36 -0500

    Added pfp, but need to debug flicker

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters';
 import Loader from 'react-loaders';
+import pfp from '../../assets/images/IMG_1889.JPG'
 
 const Home = () => {
 
@@ -20,24 +21,26 @@ const Home = () => {
     return (
     <>
         <div className="container home-page">
+        <img src={pfp} id="post-img" alt="cristian-b"/>
             <div className="text-zone">
                 <h1>
+                    <img src={pfp} id="post-img" alt="cristian-b"/>
                     <span className={`${letterClass} _11`}>H</span>
                     <span className={`${letterClass} _12`}>e</span>
                     <span className={`${letterClass} _13`}>l</span>
                     <span className={`${letterClass} _14`}>l</span>
                     <span className={`${letterClass} _15`}>o</span> 
                     <span className={`${letterClass} _16`}>,</span> 
-                 
                     <br/>
                 <AnimatedLetters letterClass={letterClass} 
                 strArray={wordArray} 
                 index={13}/> 
                 </h1>
                 <h2>
-                    <h4>New Full-Stack Software Engineer with 500+ hours of practice.</h4>
+                    <strong>New Full-Stack Software Engineer with 500+ hours of practice.</strong>
+                    <br/><br/>
                     <strong>Studied Languages/Frameworks:</strong>
-                   <em> Ruby / JavaScript / React / Ruby on Rails</em> 
+                    <em> Ruby / JavaScript / React / Ruby on Rails</em> 
                     <br/>
                 </h2>
                 <Link to="/about" className="flat-button2">ABOUT</Link>
