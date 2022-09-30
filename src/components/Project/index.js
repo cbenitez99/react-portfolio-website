4f63e33a17684f0a0f42db24081924c3fa082c02
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-09-30 17:40:22 -0500

    making progress. formatting is right; change pics next

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,12 +2,6 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters';
 import { useState, useEffect } from 'react';
 import Loader from 'react-loaders';
-// import userAuth from '../../assets/images/userAuth.png'
-// import fullCrud from '../../assets/images/fullCRUD.png'
-// import restClientEx from '../../assets/images/restclientSeeds.png'
-// import sortFunctions from '../../assets/images/sortFunctions.png'
-// import validations from '../../assets/images/validations.png'
-// import serializers2 from '../../assets/images/serializers2.png'
 import github from '../../assets/images/Screen Shot 2022-09-22 at 8.30.09 PM.png'
 
 
@@ -21,9 +15,9 @@ const Project = () => {
         }, 3000)
     }, [])
 
-    // const handleClick = () => {
-    //     window.open('https://github.com/cbenitez99/mini-pokedex-app')
-    // }
+    const handleClick = () => {
+        window.open('https://github.com/cbenitez99/mini-pokedex-app')
+    }
     // const handleApiClick = () => {
     //     window.open('https://pokeapi.co/')
     // }
@@ -39,9 +33,13 @@ const Project = () => {
                     index={15}/>
                 </h1>
                 <div className='project-zone'>
-                    <p>Project 1</p>
-                    <p>Project 2</p>
-                    <p>Project 3</p>
+                    <p><img onClick={handleClick}alt='my-github' src={github}/></p>
+                    <p><img alt='my-github' src={github}/></p>
+                    <p><img alt='my-github' src={github}/></p> 
+                    <p><img alt='my-github' src={github}/></p> 
+                    <p><img alt='my-github' src={github}/></p> 
+                    <p><img alt='my-github' src={github}/></p> 
+
 
                     {/* <img alt='my-github' src={github}></img> */}
                     {/* <p>
